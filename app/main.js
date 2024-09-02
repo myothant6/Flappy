6d62f2d46f40fa29ea319a283ec6913c0df60564
Myo Thant <152901402+myothant6@users.noreply.github.com>
2024-09-02 12:21:58 +0630

    Add files via upload

--- app/main.js
+++ app/main.js
@@ -1,14 +1,14 @@
 const button = document.querySelector("#button");
-const hightScore = document.querySelector("#best");
+const best = document.querySelector("#best");
 
 function loop() {
-  if (localStorage.getItem("best") == null) {
-    localStorage.setItem("best", 0);
-  }
-
-  hightScore.innerHTML = "Best: " + localStorage.getItem("best");
-
-  requestAnimationFrame(loop);
+    if (localStorage.getItem("best") == null) {
+        localStorage.setItem("best", 0);
+    }
+    
+    best.innerHTML = "Best: " + localStorage.getItem("best");
+    
+    requestAnimationFrame(loop);
 }
 
 loop();
